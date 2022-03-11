b6ea80f215da56625a785b69b0728b5e0f6c0530
Amin-Aghili <aghili.amin@gmail.com>
2022-03-11 16:04:00 +0300

    dunamic add-remove item in cart

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.StoreView.as_view(), name='store'),
     path('cart/', views.CartView.as_view(), name='cart'),
     path('checkout/', views.CheckoutView.as_view(), name='checkout'),
+    path('update_item/', views.UpdateItemView.as_view(), name='update_item'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+import json
 from .models import *
 
 
 class StoreView(View):
     products = Product.objects.all()
     template_name = 'store/store.html'
-    context = {'products': products}
 
     def get(self, request):
-        return render(request, self.template_name, self.context)
+        if request.user.is_authenticated:
+            customer = request.user.customer
+            order, created = Order.objects.get_or_create(customer=customer, complete=False)
+            items = order.orderitem_set.all()
+            cartItems = order.get_cart_items
+        else:
+            items = []
+            order = {'get_cart_total': 0, 'get_cart_items': 0}
+            cartItems = order['get_cart_items']
+        context = {'products': self.products, 'cartItems': cartItems}
+        return render(request, self.template_name, context)
 
     def post(self, request):
-        return render(request, self.template_name, self.context)
+        if request.user.is_authenticated:
+            customer = request.user.customer
+            order, created = Order.objects.get_or_create(customer=customer, complete=False)
+            items = order.orderitem_set.all()
+            cartItems = order.get_cart_items
+        else:
+            items = []
+            order = {'get_cart_total': 0, 'get_cart_items': 0}
+            cartItems = order['get_cart_items']
+        context = {'products': self.products, 'cartItems': cartItems}
+        return render(request, self.template_name, context)
 
 
 class CartView(View):
@@ -23,10 +44,12 @@ class CartView(View):
             customer = request.user.customer
             order, created = Order.objects.get_or_create(customer=customer, complete=False)
             items = order.orderitem_set.all()
+            cartItems = order.get_cart_items
         else:
             items = []
             order = {'get_cart_total': 0, 'get_cart_items': 0}
-        context = {'items': items, 'order': order}
+            cartItems = order['get_cart_items']
+        context = {'items': items, 'order': order, 'cartItems': cartItems}
         return render(request, self.template_name, context)
 
     def post(self, request):
@@ -34,10 +57,12 @@ class CartView(View):
             customer = request.user.customer
             order, created = Order.objects.get_or_create(customer=customer, complete=False)
             items = order.orderitem_set.all()
+            cartItems = order.get_cart_items
         else:
             items = []
             order = {'get_cart_total': 0, 'get_cart_items': 0}
-        context = {'items': items, 'order': order}
+            cartItems = order['get_cart_items']
+        context = {'items': items, 'order': order, 'cartItems': cartItems}
         return render(request, self.template_name, context)
 
 
@@ -50,10 +75,12 @@ class CheckoutView(View):
             customer = request.user.customer
             order, created = Order.objects.get_or_create(customer=customer, complete=False)
             items = order.orderitem_set.all()
+            cartItems = order.get_cart_items
         else:
             items = []
             order = {'get_cart_total': 0, 'get_cart_items': 0}
-        context = {'items': items, 'order': order}
+            cartItems = order['get_cart_items']
+        context = {'items': items, 'order': order, 'cartItems': cartItems}
         return render(request, self.template_name, context)
 
     def post(self, request):
@@ -61,8 +88,45 @@ class CheckoutView(View):
             customer = request.user.customer
             order, created = Order.objects.get_or_create(customer=customer, complete=False)
             items = order.orderitem_set.all()
+            cartItems = order.get_cart_items
         else:
             items = []
             order = {'get_cart_total': 0, 'get_cart_items': 0}
-        context = {'items': items, 'order': order}
+            cartItems = order['get_cart_items']
+        context = {'items': items, 'order': order, 'cartItems': cartItems}
         return render(request, self.template_name, context)
+
+
+class UpdateItemView(View):
+    def get(self, request):
+        data = json.loads(request.body)
+        productId = data['productId']
+        action = data['action']
+        print(action)
+        print(productId)
+
+        return JsonResponse('Item was added to cart', safe=False)
+
+    def post(self, request):
+        data = json.loads(request.body)
+        productId = data['productId']
+        action = data['action']
+        print(action)
+        print(productId)
+
+        customer = request.user.customer
+        product = Product.objects.get(id=productId)
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+        if action == 'add':
+            orderItem.quantity = (orderItem.quantity + 1)
+        elif action == 'remove':
+            orderItem.quantity = (orderItem.quantity - 1)
+
+        orderItem.save()
+
+        if orderItem.quantity <= 0:
+            orderItem.delete()
+
+        return JsonResponse('Item was added to cart', safe=False)
